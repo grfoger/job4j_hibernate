d4492f5143090a81b4a36ccd39c30b9de35f49d5
Arhip <grfoger@gmail.com>
2022-06-24 09:06:25 +0300

    init commit

---
+++ src/main/java/ru/job4j/config/model/Car.java
@@ -0,0 +1,64 @@
+package ru.job4j.config.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String model;
+
+    private Timestamp created;
+
+    public static Car of(String model, Timestamp created) {
+        Car car = new Car();
+        car.model = model;
+        car.created = created;
+        return car;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
